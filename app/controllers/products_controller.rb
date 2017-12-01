ad8f0f242741cd70e00765abf39cd40df73102a9
RaulRoman <raulromanjr@gmail.com>
2017-11-30 19:29:46 -0500

    add route and json

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,15 @@
 class ProductsController < ApplicationController
+  def the_product
+    the_product1 = Product.first
+    the_product2 = Product.second
+  
+
+    render json: {
+    product_name: the_product1.name,
+    product_price: the_product1.price,
+    product_image: the_product1.image,
+    product_description: the_product1.description,
+    second_product: the_product2.name
+  }
+  end
 end
